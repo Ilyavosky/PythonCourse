d7d2185e5c2b2dc253f37cc82f8c2a552f6c066e
Ilyavosky <ilyacortesruiz@gmail.com>
2026-03-29 12:09:16 -0600

    Casteo de tipo

--- hello.py
+++ hello.py
@@ -4,3 +4,16 @@ print("30")
 
 nombre_usuario = input("Escribe tu nombre: ")
 print(nombre_usuario)
+
+
+#Casting para tener un tipo Integer
+edad_usuario = int(input("Escribe tu edad: "))
+print(edad_usuario)
+
+#Mostrar que el tipo de dato cambió a int
+print(type(edad_usuario))
+
+#Casting para tener un tipo Float
+altura_usuario = float(input("Escribe tu altura: "))
+print(altura_usuario)
+
