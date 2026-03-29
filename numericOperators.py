9cbccd6b1dcffc9be36d889431630d62b01e4f60
Ilyavosky <ilyacortesruiz@gmail.com>
2026-03-29 12:02:54 -0600

    Añadir ejemplos de operadores

---
+++ numericOperators.py
@@ -0,0 +1,19 @@
+#Operadores Númericos
+a = 10
+b = 5
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a % b)
+print(a ** b)
+
+#Operadores de Comparación
+print(a == b)
+print(a != b)
+print(a > b)
+print(a < b)
+print(a >= b)
+print(a <= b)
